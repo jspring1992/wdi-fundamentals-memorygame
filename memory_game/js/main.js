756e89b4af5c1129bf788d18757648e1d677f73d
{Jeff} <{jeffreyspringwater@gmail.com}>
2020-02-19 19:20:46 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,13 @@
+console.log("Up and running!");
+
+let cardOne = "King";
+
+let cardTwo = "Queen";
+
+let cardThree = "King";
+
+let cardFour = "Queen";
+
+console.log("User flipped " + cardTwo);
+
+console.log("User flipped " + cardThree);
